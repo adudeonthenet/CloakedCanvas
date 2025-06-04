dbe16dbe3b833ff95a78a0015d5b130091f6cd2a
adudeonthenet <thatdudeonthenet@gmail.com>
2025-06-04 10:32:48 -0400

    Fix TS compile errors

---
+++ web/pkg/cloakedcanvas_core.d.ts
@@ -0,0 +1,2 @@
+export function decrypt_data(key: Uint8Array, data: Uint8Array, nonce: Uint8Array): Uint8Array;
+export default function init(): Promise<void>;
---
+++ web/ts/types.d.ts
@@ -0,0 +1,4 @@
+declare module '../pkg/cloakedcanvas_core.js' {
+  export function decrypt_data(key: Uint8Array, data: Uint8Array, nonce: Uint8Array): Uint8Array;
+  export default function init(): Promise<void>;
+}
